41fe3aa0f39fa75e632a404a3047a582048b964a
MaksimStriga <55689086+MaksimStriga@users.noreply.github.com>
2019-09-26 01:56:49 +0300

    Task1

---
+++ ItAcademyHomeWorks/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //явное преобразование типов
+            Int16 i1 = 1;
+            Int32 i2;
+            i2 = i1;
+            float f1;
+            f1 = i2;
+            double t34;
+            t34 = f1;
+            //неявное преобразование типов
+            double a1 = 1.1;
+            float g3;
+            g3=(float)a1;
+            double t5 = 7.7;
+            int r3 = (int)t5;
+            long y3 = 500;
+            short u3;
+            u3 = (short)y3;
+            //упаковка
+            object s1 = 123;
+            //распаковка
+            int k7 = 6;
+            object p9;
+            p9 = k7;
+
+
+        }
+    }
+}
