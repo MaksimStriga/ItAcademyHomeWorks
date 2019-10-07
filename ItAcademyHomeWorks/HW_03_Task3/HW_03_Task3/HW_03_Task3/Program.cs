a55e65313815cdf410a4096f61d7c05b7a8279c9
MaksimStriga <55689086+MaksimStriga@users.noreply.github.com>
2019-10-08 00:08:57 +0300

    Task4

--- ItAcademyHomeWorks/HW_03_Task3/HW_03_Task3/HW_03_Task3/Program.cs
+++ ItAcademyHomeWorks/HW_03_Task3/HW_03_Task3/HW_03_Task3/Program.cs
@@ -21,10 +21,21 @@ namespace HW_03_Task3
             Console.WriteLine("Введите число 2");
             string s2 = Console.ReadLine();
             int i2 = Convert.ToInt32(s2);
+            Console.WriteLine("Введите оператор");
+            string op = Console.ReadLine();
             Console.WriteLine("Введите результат");
             string s3 = Console.ReadLine();
             int i3 = Convert.ToInt32(s3);
-            if ((i1 + i2) == i3)
+            int result = 0;
+            if (op == "+")
+            {
+                result = i1 + i2;
+            }
+            else if (op == "-")
+            {
+                result = i1 - i2;
+            }
+            if (result == i3)
             {
                 Console.WriteLine("Правда");
             }
@@ -32,11 +43,11 @@ namespace HW_03_Task3
             {
                 Console.WriteLine("Не правда");
             }
-            if (i3 > (i1 + i2))
+            if (i3 > result)
             {
                 Console.WriteLine("Должно быть меньше");
             }
-            else if (i3 < (i1 + i2))
+            else if (i3 < result)
             {
                 Console.WriteLine("Должно быть больше");
             }
