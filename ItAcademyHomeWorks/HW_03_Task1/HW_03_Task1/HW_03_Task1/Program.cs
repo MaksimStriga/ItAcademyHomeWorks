3d27ec6883a383daf5e2edc26db56d1b7bd32f90
MaksimStriga <55689086+MaksimStriga@users.noreply.github.com>
2019-10-07 23:29:36 +0300

    Task1

---
+++ ItAcademyHomeWorks/HW_03_Task1/HW_03_Task1/HW_03_Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_03_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Результат" + Calculate());
+            Console.ReadKey();
+        }
+        static int Calculate()
+        {
+            Console.WriteLine("Введите число 1");
+            string s1 = Console.ReadLine();
+            int i1 = Convert.ToInt32(s1);
+            Console.WriteLine("Введите число 2");
+            string s2 = Console.ReadLine();
+            int i2 = Convert.ToInt32(s2);
+            return i1 + i2;
+        }
+    }
+}
