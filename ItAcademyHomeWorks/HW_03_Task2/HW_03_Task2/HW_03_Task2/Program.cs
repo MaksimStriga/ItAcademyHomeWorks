b8b5f6db0724ec79dcf40afbdb19bca7126d703e
MaksimStriga <55689086+MaksimStriga@users.noreply.github.com>
2019-10-07 23:43:47 +0300

    Task2

---
+++ ItAcademyHomeWorks/HW_03_Task2/HW_03_Task2/HW_03_Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_03_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Calculate() == true)
+            {
+                Console.WriteLine("Правда");
+            }
+            else
+            {
+                Console.WriteLine("Не правда");
+            }
+            Console.ReadKey();
+        }
+        static bool Calculate()
+        {
+            Console.WriteLine("Введите число 1");
+            string s1 = Console.ReadLine();
+            int i1 = Convert.ToInt32(s1);
+            Console.WriteLine("Введите число 2");
+            string s2 = Console.ReadLine();
+            int i2 = Convert.ToInt32(s2);
+            Console.WriteLine("Введите результат");
+            string s3 = Console.ReadLine();
+            int i3 = Convert.ToInt32(s3);
+            return (i1 + i2)==i3;
+        }
+    }
+}
